89ac726a3ffa7dffe1b00bdc2bc2dd0e022da64a
Allan Marina <allanmarina33@gmail.com>
2020-10-08 13:42:03 -0700

    Modified Rufinos original project

--- src/carranza/Main.java
+++ src/carranza/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello my name is Rufino!");
 	System.out.println("Hello again");
+	System.out.println("Hello Rufy, my name is Allan. ");
     }
 }
