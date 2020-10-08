77da42b2de3885ff06b850ba55026a52825f7ddd
danielperezcera <per17059@byui.edu>
2020-10-08 19:03:51 -0400

    Modification for Rufino's project

--- src/carranza/Main.java
+++ src/carranza/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	System.out.println("Hello my name is Rufino!");
 	System.out.println("Hello again");
 	System.out.println("Hello Rufy, my name is Allan. ");
+	System.out.println("Hello Rufino and Allan, my name is Daniel.");
     }
 }
